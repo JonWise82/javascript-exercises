44aebcc9056ebda027480a7336c26c40174f33d5
JonWise82 <133918862+JonWise82@users.noreply.github.com>
2023-09-22 21:00:36 +0100

    Exercise up to 06 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(nMin, nMax) {
+    
+    if (typeof nMin !== 'number' || typeof nMax !== 'number' || nMin < 0 || nMax < 0) {
+        return "ERROR";
+    }
+    
+    let total = 0;
+    let sumArray = [nMin, nMax];
+    
+    sumArray.sort((a, b) => a - b);
+    console.log(sumArray);
 
+    for (let i = sumArray[0]; i<= sumArray[1]; i++)   {
+        total += i;
+    }
+    
+    console.log(total);
+    return total;
 };
+
+//test call
+const result = sumAll(3, 5);
+console.log(sumAll);
 
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
-};
+    let result = false;
+    
+    if (year % 4 === 0) {
+        result = true;
+    }
+    
+    if (year % 100 === 0 && year % 400 !== 0) {
+        result = false;
+    }
+
+    return result;
+}
+
+console.log(leapYears(1997));
 
 // Do not edit below this line
 module.exports = leapYears;
